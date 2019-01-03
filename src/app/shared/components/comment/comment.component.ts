f0ef78dd22fcb0557214ec88c6f22ea568845d72
Andrew Young <young.andrew2@outlook.com>
2019-01-02 21:54:32 -0500

    Refactored css to utilize scss to generate dependent classes

--- src/app/shared/components/comment/comment.component.ts
+++ src/app/shared/components/comment/comment.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.scss']
+  styleUrls: ['./comment.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CommentComponent implements OnInit {
 
