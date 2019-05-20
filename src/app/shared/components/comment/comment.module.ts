1aa89979cfaaea23386857f995ef5ceb0c257de2
Andrew Young <young.andrew2@outlook.com>
2019-05-20 09:49:10 -0400

    Refactor comment module

--- src/app/shared/components/comment/comment.module.ts
+++ src/app/shared/components/comment/comment.module.ts
@@ -7,12 +7,6 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import { CommentComponent } from './comment.component';
 import { ThreadlinesComponent } from './threadlines/threadlines.component';
 import { ContentComponent } from './content/content.component';
-import { BodyComponent } from './content/body/body.component';
-import { HeaderComponent } from './content/header/header.component';
-import { FooterComponent } from './content/footer/footer.component';
-import { MoreRepliesComponent } from './more-replies/more-replies.component';
-import { ExpandButtonComponent } from './content/expand-button/expand-button.component';
-import { VoteButtonsComponent } from './content/vote-buttons/vote-buttons.component';
 import { NumberSuffixPipe } from '../../pipes/number-suffix.pipe';
 import { ReplyBoxComponent } from '../reply-box/reply-box.component';
 import { SortDropdownComponent } from '../dropdowns/sort-dropdown/sort-dropdown.component';
@@ -23,12 +17,6 @@ import { CommentSkeletonScreenComponent } from './comment-skeleton-screen/commen
         CommentComponent,
         ThreadlinesComponent,
         ContentComponent,
-        HeaderComponent,
-        BodyComponent,
-        FooterComponent,
-        MoreRepliesComponent,
-        ExpandButtonComponent,
-        VoteButtonsComponent,
         NumberSuffixPipe,
         ReplyBoxComponent,
         SortDropdownComponent,
